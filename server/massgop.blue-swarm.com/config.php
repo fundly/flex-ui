1e0daf20c303bf162b19d44359e7dbd3e1a95dac
james <james@8460f43b-7057-0410-863c-9c3c6cbb8f57>
2009-08-13 22:04:14 +0000

    Fixed the height of the widget page to remove the scrollbars.

--- server/massgop.blue-swarm.com/config.php
+++ server/massgop.blue-swarm.com/config.php
@@ -44,7 +44,7 @@ $bgColor = '#000000';
 */
 $donationWidget = array (
 	'css'			=> 'default.css',
-	'height'		=> '1100',
+	'height'		=> '1310',
 	'width'			=> '870',
 	'background'	=> '#FFFFFF',
 	'mastheadImg'	=> 'logo_large.png'
